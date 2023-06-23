1883a633acf14df4c777c9a95b3e6ba615edc2b6
Jethro-Rabbi Marcial Villena <xeqtioner10@gmail.com>
2023-06-23 21:21:17 +0800

    added background color

--- days.js
+++ days.js
@@ -51,3 +51,24 @@ buttonTira.addEventListener("click", () => {
     tiraDays();
   }, 1000);
 });
+
+
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const kolor = document.querySelector(".kolor");
+
+btn.addEventListener('click', function(){
+  let hexKolor = '#';
+  for (let i = 0; i < 6; i++) {
+    hexKolor += hex[getRandomNumber()];
+  }
+
+  kolor.textContent = hexKolor;
+  document.body.style.backgroundColor = hexKolor;
+});
+
+function getRandomNumber()  {
+  return Math.floor(Math.random() * hex.length);
+}
